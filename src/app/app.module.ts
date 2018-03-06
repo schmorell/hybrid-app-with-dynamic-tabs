688351df0b9b2d9862d5d47f52022c205ce582e5
Felix Schmorell <felix.schmorell@dialog.ch>
2018-03-06 19:54:05 +0100

    ng2 dynamic tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PersonEditComponent } from './person-edit/person-edit.component';
+import { PeopleListComponent } from './people-list/people-list.component';
+import { TabComponent } from './tab/tab.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { DynamicTabsDirective } from './dynamic-tabs.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonEditComponent,
+    PeopleListComponent,
+    TabComponent,
+    TabsComponent,
+    DynamicTabsDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ TabComponent ]
 })
 export class AppModule { }
